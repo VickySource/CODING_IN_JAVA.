5c67510cd9ea36e6b5c95985c06c51002cc95d09
Vicky <vvickyar25@gmail.com>
2025-03-28 17:55:04 +0530

    Update Operaters.java

--- Operaters.java
+++ Operaters.java
@@ -23,7 +23,7 @@ public class Operaters
     public static void Assignmentoperater(int totalScore)
     {
         //assiging data into answer from TotalScore
-        int answer = totalScore;
+        int answer = 2;
         System.out.println("Answer : " +answer);
 
         //Addition with assignment operator
@@ -67,10 +67,45 @@ public class Operaters
         System.out.println("TommoatoPrice != CarrotPrice : " +result);
 
     }
+    //Logical Operators in Java
+    static void LogicalOperaters(String Name, String Name2)
+    {
+        //Logical AND
+        boolean result = (Name == "Vicky") && (Name2 == "Vicky");
+        System.out.println("Name == Vicky && Name2 == Vicky : " +result);
+
+        //Logical OR
+        result = (Name == "Vicky") || (Name2 == "Vicky");
+        System.out.println("Name == Vicky || Name2 == Vicky : " +result);
+
+        //Logical NOT
+        result = !(Name == "Vicy");
+        System.out.println("!(Name == Vicky) : " +result);
+    }
+    //Bitwise Operators in Java
+    static void BitwiseOperaters(int a, int b)
+    {
+        // a = 5;  // 0101 in binary
+        //b = 3;  // 0011 in binary
+    
+            System.out.println("Bitwise AND: " + (a & b));  // 1  (0001)
+            System.out.println("Bitwise OR: " + (a | b));   // 7  (0111)
+            System.out.println("Bitwise XOR: " + (a ^ b));  // 6  (0110)
+            System.out.println("Bitwise NOT (~a): " + (~a)); // -6
+            System.out.println("Left Shift (a << 1): " + (a << 1));  // 10  (1010)
+            System.out.println("Right Shift (a >> 1): " + (a >> 1)); // 2  (0010)
+    }
+
+     
+    
+
     public static void main(String []args)
     {
+        //invokig all the methods at the main method 
         //ArithmaticOperater(2, 2);
         //Assignmentoperater(2);
-        RelationalOperater(50, 50);
+        //RelationalOperater(50, 50);
+        //LogicalOperaters("Vicky", "Vicky");
+        BitwiseOperaters(5, 3);
     }
 }
